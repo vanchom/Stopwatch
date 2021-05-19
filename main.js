0807987b5c567f2170b45f877e8fb0b6bb50313c
Ivan Molina <ivann.molinad@gmail.com>
2021-05-19 11:06:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var h2 = document.getElementsByTagName("h2")[0],
+    start = document.getElementById("start"),
+    pause = document.getElementById("pause"),
+    stop = document.getElementById("stop"),
+    reset =document.getElementById("reset"),
+    seconds= 0, minutes =0, hours=0, t=0;
+
+    function add(){
+        seconds++;
+        if (seconds >=60){
+            seconds =0;
+            minutes++;
+            if(minutes >= 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+        h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+        timer();
+    }
+    function timer() {
+        
+        t = setTimeout(add, 1000);
+    }
+    
+    
+
+    /* Start button */
+start.onclick = function(){
+    timer(t);
+     document.querySelector(".values").style.color = "green";
+}
+
+// pause button
+pause.onclick = function(){
+    setTimeout(t);
+    document.querySelector(".values").style.color = "orange";
+}
+
+/* Stop button */
+stop.onclick = function() {
+    clearTimeout(t);
+    document.querySelector(".values").style.color = "red";
+}
+/* Clear button */
+reset.onclick = function() {
+    h2.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+    document.querySelector(".values").style.color = "black";
+}
